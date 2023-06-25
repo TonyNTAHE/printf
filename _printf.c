2a1ed018a220f34d348049dfed7a78a2c6f0a5d9
TonyNTAHE <tonyntahe@gmail.com>
2023-06-26 01:08:13 +0200

    _printf.c file

--- _printf.c
+++ _printf.c
@@ -2,69 +2,53 @@
 
 /**
  * _printf - produces output according to a format.
- * @format - format.
+ * @format: format.
  * Return: the number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-        va_list arg;
-        char c;
-        char *s;
-        int count = 0;
+	va_list ap;
+	int len  = 0, i = 0;
+	char c, *s;
 
-        va_start(arg, format);
-        if (format)
-        {
-                while(*format)
-                {
-                        if (*format == '%')
-                        {
-                                format++;
-                                switch(*format)
-                                {
-                                        case 'c':
-                                                c = va_arg(arg, int);
-                                                _putchar(c);
-                                                count++;
-                                                break;
-                                        case 's':
-                                                s = va_arg(arg, char *);
-                                                if (*s)
-                                                {
-                                                        while (*s)
-                                                        {
-                                                                _putchar(*s);
-                                                                count++;
-                                                                s++;
-                                                        }
-                                                        break;
-                                                }
-                                                else
-                                                        return (-1);
-                                                break;
-                                        case '%':
-                                                _putchar('%');
-                                                count++;
-                                                break;
-                                        default:
-                                                _putchar('%');
-                                                _putchar(*format);
-                                                count += 2;
-                                                break;
-                                }
-                        }
-                        else
-                        {
-                                _putchar(*format);
-                                count++;
-                        }
-                        format++;
-                }
-        }
-        else
-        {
-                return (-1);
-        }
-        va_end(arg);
-        return (count);
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			++i;
+			if (format[i] == 'c')
+			{
+				c = (char)va_arg(ap, int);
+				_putchar(c);
+				len++;
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(ap, char *);
+				if (s)
+				{
+					while (*s)
+					{
+						_putchar(*s);
+						len++;
+						s++;
+					}
+				}
+			}
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				len++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		i++;
+	}
+	va_end(ap);
+	return (len);
 }
