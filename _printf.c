1bd4b376a211f3d762e0a2932dca724444481c43
TonyNTAHE <tonyntahe@gmail.com>
2023-06-26 11:41:14 +0200

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * _printf  - produces output according to a format.
+ * @format: format.
+ * Return: the number pf characters to a format.
+ */
 int _printf(const char *format, ...)
 {
 va_list fmt;
@@ -9,36 +14,32 @@ char *s;
 va_start(fmt, format);
 if (format)
 {
-for (; *format; format++)
-{
-if (*format == '%' && *(format + 1))
-{
-if (*format == 'c')
-len += _putchar(va_arg(fmt, int));
-else if (*format == 's')
-{
-s = va_arg(fmt, char *s);
-if (s)
-{
-while (*s)
-len += _putchar(*s++);
-}
-else
-return (-1);
-}
-else if (*format == '%')
-len += _putchar('%');
-else
-len += _putchar('%') + _putchar(*format);
-}
-else if (*format == '%' && !*(format + 1))
-{
-va_end(fmt);
-return (-1);
-}
-else
-len += _putchar(*format);
-}
+	for (; *format; format++)
+	{
+		if (*format == '%' && *(++format))
+		{
+			if (*format == 'c')
+
+				len += _putchar(va_arg(fmt, int));
+			else if (*format == 's')
+			{
+				s = va_arg(fmt, char *);
+				if (s)
+				{
+					while (*s)
+						len += _putchar(*s++);
+				}
+				else
+					return (-1);
+			}
+			else if (*format == '%')
+				len += _putchar('%');
+			else
+				len += _putchar('%') + _putchar(*format);
+		}
+		else
+			len += _putchar(*format);
+	}
 }
 else
 return (-1);
