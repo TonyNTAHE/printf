cd67d21efafca81c71d4bed7dc6fee8f3286051d
TonyNTAHE <tonyntahe@gmail.com>
2023-06-26 03:44:29 +0200

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -14,39 +14,31 @@ char *s;
 va_start(fmt, format);
 if (format)
 {
-while (*format)
-{
-if (*format == '%' && *(++format))
-{
-switch (*format)
-{
-case 'c':
-len += _putchar(va_arg(fmt, int));
-break;
-case 's':
-s = va_arg(fmt, char *);
-if (s)
-{
-while (*s)
-len += _putchar(*s++);
-}
-else
-return (-1);
-break;
-case '%':
-len += _putchar('%');
-break;
-case '\0':
-return (-1);
-default:
-len += _putchar('%') + _putchar(*format);
-break;
-}
-}
-else
-len += _putchar(*format);
-format++;
-}
+	for (; *format; format++)
+	{
+		if (*format == '%' && *(++format))
+		{
+			if (*format == 'c')
+				len += _putchar(va_arg(fmt, int));
+			else if (*format == 's')
+			{
+				s = va_arg(fmt, char *);
+				if (s)
+				{
+					while (*s)
+						len += _putchar(*s++);
+				}
+				else
+					return (-1);
+			}
+			else if (*format == '%')
+				len += _putchar('%');
+			else
+				len += _putchar('%') + _putchar(*format);
+		}
+		else
+			len += _putchar(*format);
+	}
 }
 else
 return (-1);
