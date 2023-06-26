ac4db87e9d33dbae2e0d9b9ab66d31ac0151abf2
TonyNTAHE <tonyntahe@gmail.com>
2023-06-26 04:02:46 +0200

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -9,36 +9,42 @@ int _printf(const char *format, ...)
 {
 va_list fmt;
 int len = 0;
-char *s;
+char c, *s;
 
 va_start(fmt, format);
 if (format)
 {
-	for (; *format; format++)
-	{
-		if (*format == '%' && *(++format))
-		{
-			if (*format == 'c')
-				len += _putchar(va_arg(fmt, int));
-			else if (*format == 's')
-			{
-				s = va_arg(fmt, char *);
-				if (s)
-				{
-					while (*s)
-						len += _putchar(*s++);
-				}
-				else
-					return (-1);
-			}
-			else if (*format == '%')
-				len += _putchar('%');
-			else
-				len += _putchar('%') + _putchar(*format);
-		}
-		else
-			len += _putchar(*format);
-	}
+for (; *format; format++)
+{
+if (*format == '%' && *(++format))
+{
+if (*format == 'c')
+c = va_arg(fmt, int);
+if (c)
+{
+len += _putchar(va_arg(fmt, int));
+}
+else
+return (-1);
+else if (*format == 's')
+{
+s = va_arg(fmt, char *);
+if (s)
+{
+while (*s)
+len += _putchar(*s++);
+}
+else
+return (-1);
+}
+else if (*format == '%')
+len += _putchar('%');
+else
+len += _putchar('%') + _putchar(*format);
+}
+else
+len += _putchar(*format);
+}
 }
 else
 return (-1);
